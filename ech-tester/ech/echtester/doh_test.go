3f35ca17f9463ee889e5dffcd60cf65af92d7940
huanfeng123 <56348872+huanfeng123@users.noreply.github.com>
2026-07-30 18:50:09 +0800

    test: correct HTTPS wire record fixture

--- ech-tester/ech/echtester/doh_test.go
+++ ech-tester/ech/echtester/doh_test.go
@@ -3,7 +3,7 @@ package echtester
 import "testing"
 
 func TestExtractECHFromWireHTTPSRecord(t *testing.T) {
-	data := `\# 17 00 01 00 00 00 05 02 68 33 00 05 00 04 01 02 03 04`
+	data := `\# 11 00 01 00 00 05 00 04 01 02 03 04`
 	config, err := extractECH(data)
 	if err != nil {
 		t.Fatal(err)
